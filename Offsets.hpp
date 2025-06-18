1d555b3cb94959cedb9db072834c4abdd613307b
andreux2025 <turandreu124@gmail.com>
2025-06-18 14:16:57 -0800

    more uffsuts

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,14 +1,16 @@
-namespace Offsets // made by andreu
+namespace Offsets // made by andreu fixed dumper 7 size trust my ass
 {
 uintptr_t Uworld = 0x5526FC0; // gworld/uworld for retardeds
 uintptr_t Actors = 0xA0; // world esp leak incoming!
-uintptr_t ProcessEvent = 0x182D530;
+uintptr_t ProcessEvent = 0x182D530;// made by andreu fixed dumper 7 size trust my ass
 uintptr_t ProcessEventIdx = 0x00000040; //THIS / 8 = INDEX 0x8?
-uintptr_t GNames = 0x5420C50;
+uintptr_t GNames = 0x5420C50;// made by andreu fixed dumper 7 size trust my ass
 uintptr_t AcknowledgedPawn = 0x3B0; // local pawn for retardeds
-uintptr_t PlayerCameraManager = 0x3C8;
-uintptr_t Mesh = 0x388;
-uintptr_t OwningGameInstance = 0x190;
+uintptr_t PlayerCameraManager = 0x3C8;// made by andreu fixed dumper 7 size trust my ass
+uintptr_t Mesh = 0x388;// made by andreu fixed dumper 7 size trust my ass
+uintptr_t OwningGameInstance = 0x190;// made by andreu fixed dumper 7 size trust my ass
 uintptr_t LocationUnderReticle = 0x1630; // triggerbot
-uintptr_t OwningGameInstance = 0x190;
+uintptr_t OwningGameInstance = 0x190;// made by andreu fixed dumper 7 size trust my ass
+uintptr_t TeamIndex = 0xB90;// made by andreu fixed dumper 7 size trust my ass
+uintptr_t Specator = 0x356;// made by andreu fixed dumper 7 size trust my ass
 }
